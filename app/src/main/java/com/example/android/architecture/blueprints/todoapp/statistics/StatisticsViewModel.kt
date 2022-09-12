9f2e850134b22e4e6fceb88261b7a97442514205
Gozirin <chigozp2@gmail.com>
2022-09-13 00:05:29 +0100

    first commit

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModel.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright (C) 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.example.android.architecture.blueprints.todoapp.statistics
 
@@ -45,7 +31,8 @@ class StatisticsViewModel(application: Application) : AndroidViewModel(applicati
     }
 
     val activeTasksPercent = stats.map {
-        it?.activeTasksPercent ?: 0f }
+        it?.activeTasksPercent ?: 0f
+    }
     val completedTasksPercent: LiveData<Float> = stats.map { it?.completedTasksPercent ?: 0f }
     val dataLoading: LiveData<Boolean> = _dataLoading
     val error: LiveData<Boolean> = tasks.map { it is Error }
@@ -53,9 +40,9 @@ class StatisticsViewModel(application: Application) : AndroidViewModel(applicati
 
     fun refresh() {
         _dataLoading.value = true
-            viewModelScope.launch {
-                tasksRepository.refreshTasks()
-                _dataLoading.value = false
-            }
+        viewModelScope.launch {
+            tasksRepository.refreshTasks()
+            _dataLoading.value = false
+        }
     }
 }
